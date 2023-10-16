15f75b7b7688a0db620e042dd033e9b8dc7f92f0
JLipnerPitt <122459494+JLipnerPitt@users.noreply.github.com>
2023-10-16 17:46:16 -0400

    Add files via upload
    Skeleton for the slotmachinebopit class

---
+++ slotmachinebopit.h.ino
@@ -0,0 +1,23 @@
+#ifndef SlotMachineBopIt_h
+#define SlotMachineBopIt_h
+#include Arduino.h
+class SlotMachineBopIt {
+  public:
+    SlotMachineBopIt(int pin);
+    bool coinVerifier(int pin);
+    void actions(int);
+    void slots();
+    void roulette();
+    void pachinko();
+    int getScore();
+    void setScore();
+    bool getGameState();
+    
+
+  private:
+    bool game_start;
+    int lever_pin;
+    int wheel_pin;
+    int joystick_pin;
+};
+#endif
