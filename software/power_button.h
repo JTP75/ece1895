ccd9b150361f900ed65586fd8d997dfb7db66e85
Justin <JTP75@pitt.edu>
2023-10-20 12:14:06 -0400

    state machine work

--- software/power_button.h
+++ software/power_button.h
@@ -1,4 +1,13 @@
 #pragma once
 
+#ifdef ARDUINO
+#include <Arduino.h>
+#endif
+
 class PowerButton {
+    private:
+        int pin;
+    public:
+        PowerButton(int pin);
+        bool is_pressed();
 };
